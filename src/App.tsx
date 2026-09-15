45300eaa23b8eb0eb1769d1bc55ead1c4b7551f4
Hutaba-Makura <152959976+Hutaba-Makura@users.noreply.github.com>
2026-09-15 17:55:13 +0900

    Add:　Xリンクを追加

--- src/App.tsx
+++ src/App.tsx
@@ -34,6 +34,17 @@ export default function App() {
         <ColorHistory history={colorHistory.history} onSelect={commitColor} />
       </div>
     </main>
+      <a
+        className={styles.authorLink}
+        href="https://x.com/Mars_Neobase"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <svg xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" viewBox="0 0 16 16">
+          <path d="M0 0h16v16H0z" fill="none" />
+          <path fill="currentColor" d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07l-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865z" />
+        </svg>
+      </a>
     </div>
   );
 }
